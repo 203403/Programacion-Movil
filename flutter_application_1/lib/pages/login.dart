29bfaf8b7a035e602b16af68d80219ca14b28ff0
Omar Franco <203403@ids.upchiapas.edu.mx>
2023-02-14 04:15:05 -0600

    update (login): add the app bar and update the button to be able to use navigator

--- flutter_application_1/lib/pages/login.dart
+++ flutter_application_1/lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -6,6 +7,24 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Color(0xffe53863)),
+        title: const Text('Login'),
+        elevation: 0,
+        leadingWidth: 60,
+        titleSpacing: -10,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: SizedBox(
+              width: 70,
+              child: Image.asset(
+                'assets/images/OutsideLogin.png',
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.only(left: 20, right: 20),
@@ -17,14 +36,42 @@ class Login extends StatelessWidget {
                       BoxConstraints(minHeight: viewportConstrains.maxHeight),
                   child: Padding(
                     padding: const EdgeInsets.only(top: 300),
-                    child: Column(children: const [
-                      Padding(
+                    child: Column(children: [
+                      const Padding(
                           padding: EdgeInsets.only(bottom: 20),
                           child: TextField(
                             decoration: InputDecoration(hintText: 'Username'),
                           )),
-                      TextField(
-                        decoration: InputDecoration(hintText: 'Password'),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Password'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: RichText(
+                          textWidthBasis: TextWidthBasis.longestLine,
+                          textAlign: TextAlign.justify,
+                          text: TextSpan(
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.black),
+                            children: [
+                              const TextSpan(
+                                  text: "¿No recuerdas tu contraseña? "),
+                              TextSpan(
+                                text: "Recuperar contraseña",
+                                style: const TextStyle(
+                                    color: Color(0xffe73863),
+                                    fontWeight: FontWeight.bold),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () async {
+                                    Navigator.pushNamed(context, '/recovery1');
+                                  },
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ]),
                   ),

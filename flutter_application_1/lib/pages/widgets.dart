9603ae74360808ee670a007b7c697a67a8c0fe20
Omar Franco <203403@ids.upchiapas.edu.mx>
2023-03-06 11:26:48 -0600

    feat: create widgets to show alerts

---
+++ flutter_application_1/lib/pages/widgets.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> alert(BuildContext context, String title, String content) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<dynamic> alertToView(
+    BuildContext context, String title, String content, String view) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, view);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<dynamic> alertToViewWithParams(
+    BuildContext context, String title, String content, String view, params) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, view, arguments: params);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

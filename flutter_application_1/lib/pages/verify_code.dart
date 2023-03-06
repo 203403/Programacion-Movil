b5fa935931abee86e216afcfece957e25a5ce9f7
Omar Franco <203403@ids.upchiapas.edu.mx>
2023-03-06 12:15:40 -0600

    feat (verify code): create the verify code

---
+++ flutter_application_1/lib/pages/verify_code.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/auth_request.dart';
+
+class VerifyCode extends StatefulWidget {
+  const VerifyCode({super.key});
+
+  @override
+  State<VerifyCode> createState() => _VerifyCodeState();
+}
+
+class _VerifyCodeState extends State<VerifyCode> {
+  final linkController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final dynamic args = ModalRoute.of(context)!.settings.arguments;
+    // final params = args['params'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Verificar código"),
+            Image.asset(
+              "assets/images/mini_logo.png",
+              width: 70,
+              height: 70,
+            ),
+          ],
+        ),
+        backgroundColor: const Color(0xff48197c),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25, right: 25),
+          child: LayoutBuilder(
+            builder:
+                (BuildContext context, BoxConstraints viewportConstraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: viewportConstraints.maxHeight,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Text(
+                            "Ingresa en el siguiente campo el enlace de recuperación de contraseña que se te fue enviado a su dirección de correo electrónico.",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 113, 113, 113),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 30, bottom: 5),
+                          child: Text(
+                            "Enlace de recuperacion",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                        ),
+                        TextField(
+                          controller: linkController,
+                          // obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            hintText: ('Enlace de recuperacion'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 50),
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: 45,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                shape: (RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                )),
+                                side: const BorderSide(
+                                  width: 2,
+                                  color: Colors.green,
+                                ),
+                              ),
+                              child: const Text(
+                                "Verificar enlace",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              onPressed: () {
+                                verifyCode(context, linkController.text, args);
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}

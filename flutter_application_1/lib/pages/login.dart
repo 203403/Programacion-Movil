1ae70cfab99dfadefc223613e9e3f9ab65959c82
Omar Franco <203403@ids.upchiapas.edu.mx>
2023-03-06 11:26:06 -0600

    feat (login): add the services to login with email and password

--- flutter_application_1/lib/pages/login.dart
+++ flutter_application_1/lib/pages/login.dart
@@ -1,79 +1,161 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/auth_request.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(color: Color(0xffe53863)),
-        title: const Text('Login'),
-        elevation: 0,
-        leadingWidth: 60,
-        titleSpacing: -10,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 15.0),
-            child: SizedBox(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Iniciar Sesión"),
+            Image.asset(
+              "assets/images/mini_logo.png",
               width: 70,
-              child: Image.asset(
-                'assets/images/OutsideLogin.png',
-              ),
+              height: 70,
             ),
-          ),
-        ],
+          ],
+        ),
+        backgroundColor: const Color(0xff48197c),
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
+          padding: const EdgeInsets.only(left: 25, right: 25),
           child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints viewportConstrains) {
+            builder:
+                (BuildContext context, BoxConstraints viewportConstraints) {
               return SingleChildScrollView(
                 child: ConstrainedBox(
-                  constraints:
-                      BoxConstraints(minHeight: viewportConstrains.maxHeight),
+                  constraints: BoxConstraints(
+                    minHeight: viewportConstraints.maxHeight,
+                  ),
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 300),
-                    child: Column(children: [
-                      const Padding(
-                          padding: EdgeInsets.only(bottom: 20),
-                          child: TextField(
-                            decoration: InputDecoration(hintText: 'Username'),
-                          )),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                        child: TextField(
-                          decoration: InputDecoration(hintText: 'Password'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: RichText(
-                          textWidthBasis: TextWidthBasis.longestLine,
-                          textAlign: TextAlign.justify,
-                          text: TextSpan(
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.black),
-                            children: [
-                              const TextSpan(
-                                  text: "¿No recuerdas tu contraseña? "),
-                              TextSpan(
-                                text: "Recuperar contraseña",
-                                style: const TextStyle(
-                                    color: Color(0xffe73863),
-                                    fontWeight: FontWeight.bold),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () async {
-                                    Navigator.pushNamed(context, '/recovery1');
-                                  },
-                              ),
-                            ],
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            "Inicia sesión para empezar a usar la app",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 113, 113, 113),
+                            ),
                           ),
                         ),
-                      ),
-                    ]),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 30, bottom: 5),
+                          child: Text(
+                            "Correo eletrónico",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                        ),
+                        TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            hintText: ('Dirección de correo'),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 30, bottom: 5),
+                          child: Text(
+                            "Contraseña",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                        ),
+                        TextField(
+                          controller: passController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            hintText: ('Contraseña'),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/recover_pass');
+                              },
+                              child: const Text(
+                                'Recuperar contraseña',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: 45,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                shape: (RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                )),
+                                side: const BorderSide(
+                                  width: 2,
+                                  color: Colors.green,
+                                ),
+                              ),
+                              child: const Text(
+                                "Ingresar",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              onPressed: () {logInWithPassword(context, emailController.text, passController.text);},
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("¿No tienes una cuenta?"),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/register');
+                              },
+                              child: const Text(
+                                'Regístrate',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffe73f6a)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
